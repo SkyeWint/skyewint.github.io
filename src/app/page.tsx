b6c7528630516eb764e791b6b2060661f2845a74
Luna <midnyte25@gmail.com>
2025-10-16 22:46:21 -0700

    Added testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ const Home = () => {
             <div className="hidden md:block md:w-sm lg:w-2xs shrink-0">
               &nbsp;
             </div>
+
             <div className="z-1 flex flex-col drop-shadow-md drop-shadow-black md:drop-shadow-none text-center text-white md:text-sky-950 items-center gap-35 md:gap-3">
               <h1 className="text-8xl md:text-[9rem] leading-[9rem] font-normal">
                 Skyfox
@@ -25,7 +26,26 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className={`w-full flex justify-center ${pink} p-6`}>
+
+      <section
+        className={`w-full flex flex-col md:flex-row justify-center p-8 ${pink}`}
+      >
+        <HeroCard
+          href={pages.about.url}
+          imgUrl="/pic-in-frame-320.webp"
+          text="Learn About Skye"
+          pink
+        />
+
+        <HeroCard
+          href={pages.booking.url}
+          imgUrl="/journey-320.webp"
+          text="Start Your Journey"
+          pink
+        />
+      </section>
+
+      <section className={`w-full flex justify-center ${blue} p-6`}>
         <div className="w-3xl flex flex-col items-center">
           <h3 className="text-3xl text-center font-semibold tracking-wide pb-4">
             Empowering People of All Backgrounds to Thrive
@@ -35,36 +55,74 @@ const Home = () => {
             <li>
               Accommodating and empowering for LGBTQ+ and neurodiverse people
             </li>
+
             <li>Break free from unhealthy habits and living patterns</li>
+
             <li>Reach your goals in stable and sustainable ways</li>
+
             <li>
               Take back your time and money from addictive and toxic media
             </li>
           </ul>
         </div>
       </section>
-      <section className={`w-full flex justify-center ${pink} p-6`}>
-        <div className="w-3xl flex flex-col items-center text-xl text-center ">
-          <em>
-            <p className="pb-4">
-              Everyone has the ability to make their lives better, to make their
-              days brighter, to feel less stress from unfinished tasks.
-              Sometimes we all just need a little push and a little support to
-              get ourselves there.
-            </p>
-            <p className="pb-4">I can be the support you need.</p>
-          </em>
+
+      <section className={`w-full flex justify-center ${blue} p-6`}>
+        <div className="w-3xl flex flex-col gap-4 items-center font-semibold text-xl text-center ">
+          <p>
+            Everyone has the ability to make their lives better, to make their
+            days brighter, to feel less stress from unfinished tasks. Sometimes
+            we all just need a little push and a little support to get ourselves
+            there.
+          </p>
+
+          <p>
+            <em>I can be the support you need.</em>
+          </p>
         </div>
       </section>
+
+      <section className={`w-full flex justify-center ${pink} p-6`}>
+        <div className="w-3xl flex flex-col gap-4 items-center text-xl text-center ">
+          <div className="flex flex-col gap-2">
+            <p>
+              <em>
+                &quot;Seldom in life do you meet someone with as much patience
+                and understanding. Ms. Moore has helped me time and time again,
+                with a calm, kind, guiding hand for over a decade now. I was
+                quite lucky to have someone as knowledgeable and kind with me
+                when I needed it.&quot;
+              </em>
+            </p>
+
+            <p>
+              - Beckett Ellis; <em>Georgia, United States</em>
+            </p>
+          </div>
+
+          <div>
+            <p>
+              <em>
+                &quot;I was feeling stuck in a rut, but Skye&apos;s coaching
+                services helped me out of my funk. Their pragmatic approach to
+                care was really refreshing and I always felt supported.
+                It&apos;s like having your own sidekick. I made way more
+                progress on my executive dysfunction in the first 2 sessions
+                then I did in entire packages of therapy. I&apos;d recommend
+                them to anyone in a heartbeat!&quot;
+              </em>
+            </p>
+
+            <p>
+              - Cleo Andersen; <em>Washington, United States</em>
+            </p>
+          </div>
+        </div>
+      </section>
+
       <section
         className={`w-full flex flex-col md:flex-row justify-center p-8 ${blue}`}
       >
-        <HeroCard
-          href={pages.about.url}
-          imgUrl="/pic-in-frame-320.webp"
-          text="Learn About Skye"
-        />
-
         <HeroCard
           href={pages.booking.url}
           imgUrl="/journey-320.webp"
